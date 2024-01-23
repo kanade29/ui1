27aeab4157f21e748b71ab7d215848bc1b47aa8c
kanade29 <vicky.kanade29@gmail.com>
2024-01-23 15:56:24 +0530

    ui1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+// Assignment 2
+
+// take an array of different colors. Let's say red, yellow, blue, green. onClick of a specific color, the background of the UI will change to that specific color. And the color div will be changed to white.
+
+import { useState } from 'react';
 import './App.css';
 
-function App() {
+const ColorChanger = () => {
+  const colors = ['red', 'yellow', 'blue', 'green'];
+  const [selectedColor, setSelectedColor] = useState(null);
+
+  const handleColorClick = (color) => {
+    document.body.style.backgroundColor = color;
+    setSelectedColor(color); // Update to set the selectedColor to the clicked color
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="color-container">
+      {colors.map((color, index) => (
+        <div
+          key={index}
+          className="color-box"
+          style={{ backgroundColor: selectedColor === color ? 'white' : color }}
+          onClick={() => handleColorClick(color)}
         >
-          Learn React
-        </a>
-      </header>
+          {color}
+        </div>
+      ))}
     </div>
   );
-}
+};
 
-export default App;
+export default ColorChanger;
+
+
+
+
